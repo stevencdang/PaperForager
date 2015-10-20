e5ecaab373c532d3ddf7271a844e613e16c52333
Steven Dang <stevenda@cs.cmu.edu>
2015-10-20 02:53:32 -0400

    added simple search for wikipedia

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import wikipedia
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return wikipedia.summary("Wikipedia")
+
+@app.route('/<term>')
+def search_wiki(term):
+    
+    return wikipedia.page(term).content
 
 if __name__ == '__main__':
     app.run()
